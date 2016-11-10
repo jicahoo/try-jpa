5e50a9aaa17afdc10bd764f26d66a3caa64f69cc
Jichao <jicahoo@163.com>
2016-11-10 11:03:02 -0500

    First example.

---
+++ src/test/java/com/jichao/jichao/basics/JpaBasics.java
@@ -0,0 +1,52 @@
+package com.jichao.jichao.basics;
+
+import com.jichao.jpa.entity.Point;
+import org.junit.Test;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by jichao on 2016/11/10.
+ */
+public class JpaBasics {
+
+    @Test
+    public void testCrud() {
+        //http://www.objectdb.com/tutorial/jpa/start
+        // Open a database connection
+        // (create a new database if it doesn't exist yet):
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("$objectdb/db/points.odb");
+        EntityManager em = emf.createEntityManager();
+
+        // Store 1000 Point objects in the database:
+        em.getTransaction().begin();
+        for (int i = 0; i < 1000; i++) {
+            Point p = new Point(i, i);
+            em.persist(p);
+        }
+        em.getTransaction().commit();
+
+        // Find the number of Point objects in the database:
+        Query q1 = em.createQuery("SELECT COUNT(p) FROM Point p");
+        System.out.println("Total Points: " + q1.getSingleResult());
+
+        // Find the average X value:
+        Query q2 = em.createQuery("SELECT AVG(p.x) FROM Point p");
+        System.out.println("Average X: " + q2.getSingleResult());
+
+        // Retrieve all the Point objects from the database:
+        TypedQuery<Point> query =
+                em.createQuery("SELECT p FROM Point p", Point.class);
+        List<Point> results = query.getResultList();
+        for (Point p : results) {
+            System.out.println(p);
+        }
+
+        // Close the database connection:
+        em.close();
+        emf.close();
+    }
+
+}
